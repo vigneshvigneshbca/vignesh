91cf0e122121e37cfe1491faff28ae1bc78080ef
vigneshvigneshbca <145039673+vigneshvigneshbca@users.noreply.github.com>
2023-09-14 20:55:17 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,9 @@
+def factorial(n):
+  if n == 1 or n == 0:
+    return 1
+  else:
+    return n * factorial(n - 1)
+
+
+num = int(input("Entet the number : "))
+print('The factorial of {} is {}'.format(num, factorial(num)))
